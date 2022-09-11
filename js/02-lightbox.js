d71582a26c71cd4f5e47b395372d68da13b90fbb
Mykola Zaikovskyi <mykola.zaikovskyi@gmail.com>
2022-09-11 20:52:47 +0300

    add task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,40 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryRef = document.querySelector(".gallery");
+
+const galleryMarkup = createGalleryMarkup(galleryItems);
+galleryRef.insertAdjacentHTML("beforeend", galleryMarkup);
+
+galleryRef.addEventListener("click", onImgClick);
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+        <a class="gallery__item" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}" 
+            alt="${description}" />
+        </a>`;
+    })
+    .join("");
+}
+
+function onImgClick(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  var lightbox = new SimpleLightbox(".gallery a", {
+    /* options */
+  });
+ 
+
+  event.target.setAttribute("title", `${event.target.alt}`);
+}
