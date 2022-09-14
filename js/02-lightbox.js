4e56fe7a76656a2c24fa77e19cb837cd2003ea44
Mykola Zaikovskyi <mykola.zaikovskyi@gmail.com>
2022-09-14 10:24:10 +0300

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -8,31 +8,24 @@ const galleryRef = document.querySelector(".gallery");
 const galleryMarkup = createGalleryMarkup(galleryItems);
 galleryRef.insertAdjacentHTML("beforeend", galleryMarkup);
 
-galleryRef.addEventListener("click", onImgClick);
-
 function createGalleryMarkup(galleryItems) {
   return galleryItems
     .map(({ preview, original, description }) => {
       return `
+      <li>
         <a class="gallery__item" href="${original}">
           <img
             class="gallery__image"
             src="${preview}" 
             alt="${description}" />
-        </a>`;
+        </a>
+      </li>`;
     })
     .join("");
 }
 
-function onImgClick(event) {
-  event.preventDefault();
-  if (event.target.nodeName !== "IMG") {
-    return;
-  }
-
-  var lightbox = new SimpleLightbox(".gallery a", {
-    /* options */
-    captionsData: "alt",
-    captionDelay: 250,
-  });
-}
+var lightbox = new SimpleLightbox(".gallery a", {
+  /* options */
+  captionsData: "alt",
+  captionDelay: 250,
+});
